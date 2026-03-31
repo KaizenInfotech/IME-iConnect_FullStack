294612d5d70aa2e5ba69859bd63a2091259c43f9
KaizenInfotech <ios@kaizeninfotech.com>
2026-03-31 12:20:06 +0530

    base URL changed

--- imei-connect-admin/src/api/axiosInstance.js
+++ imei-connect-admin/src/api/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: (import.meta.env.VITE_API_URL || 'https://api.imeiconnect.com/V2') + '/api',
+  // baseURL: (import.meta.env.VITE_API_URL || 'https://api.imeiconnect.com/V2') + '/api',
+  baseURL: (import.meta.env.VITE_API_URL || 'https://events.yatrimitra.com/API') + '/api',
   headers: {
     'Content-Type': 'application/json',
   },
